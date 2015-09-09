782dd8d20d564420a2babcd016c47620f93efc90
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-09 12:25:03 -0700

    Initial homepage search design
    Initial homepage search design

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php 
+	/*
+	 * Add HTML5 search box
+	 */
+	add_theme_support( 'html5', array( 'header-search-input' ) );
---
+++ parts/before-main.php
@@ -0,0 +1,25 @@
+	<!-- Search interface, hidden by default until interaction in header -->
+	<div class="header-search-wrapper header-search-wrapper-hide">
+		<section class="side-left row" id="search-modal">
+			<div class="column one">
+				&nbsp;
+			</div>
+			<div class="column two">
+				<div class="header-search-input-wrapper">
+					<form method="get" action="https://search.wsu.edu/Default.aspx">
+						<div class="radio-rapper">
+							<input type="radio" name="searchRadio" class="radio"> <label>Search WSU HR Site</label> <input type="radio" name="searchRadio" class="radio"> <label>Search WSU Forms</label> <input type="radio" name="searchRadio" class="radio"> <label>Search WSU Jobs</label>
+						</div>
+						<input name="cx" value="002970099942160159670:yqxxz06m1b0" type="hidden">
+						<input name="cof" value="FORID:11" type="hidden">
+						<input name="sa" value="Search" type="hidden">
+						<label class="search-label" for="header-search">Search</label>
+
+						<input type="text" value="" name="q" placeholder="Search" class="header-search-input" />
+					</form>
+					<div class="si-dropdown">&#x25BE;</div>
+				</div>
+			</div>
+		</section>
+		<div class="close-header-search">x</div>
+	</div>
