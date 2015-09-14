77943a53c06b82acab687eeae3103fd91676edf9
jeremyfelt <jeremy.felt@gmail.com>
2015-09-14 10:43:57 -0700

    Fix spacing in functions.php

--- functions.php
+++ functions.php
@@ -1,19 +1,18 @@
 <?php 
 
-	add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
-	/*
- 	 * Enqueue custom scripting in child theme.
- 	 */
-	function enqueue_scripts() {
-		wp_enqueue_script( 'custom', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
+/*
+ * Enqueue custom scripting in child theme.
+ */
+function enqueue_scripts() {
+	wp_enqueue_script( 'custom', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
 
-	}
-	/* 
-	 * Add HTML5 search box
-	 */
-	add_action( 'after_setup_theme', 'hrs_html_support' );
-	
-	function hrs_html_support() {
-		add_theme_support( 'html5', array( 'header-search-input' ) );
-	}
-	
+}
+/*
+ * Add HTML5 search box
+ */
+add_action( 'after_setup_theme', 'hrs_html_support' );
+
+function hrs_html_support() {
+	add_theme_support( 'html5', array( 'header-search-input' ) );
+}
