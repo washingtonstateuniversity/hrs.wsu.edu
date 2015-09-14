5f40b5072044c5ab21902e239a95b31e2371e4b4
jeremyfelt <jeremy.felt@gmail.com>
2015-09-14 10:44:18 -0700

    Prefix the enqueue_scripts function

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php 
 
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'hrs_enqueue_scripts');
 /*
  * Enqueue custom scripting in child theme.
  */
-function enqueue_scripts() {
+function hrs_enqueue_scripts() {
 	wp_enqueue_script( 'custom', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
 
 }
