f3552241aa1f61d58d5eb0d9aa3509f7eaf9b5e4
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-17 13:58:56 -0700

    Testing my authentication
    Testing my authentication
    Plus js upload
    Functions.php enque

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ add_action( 'wp_enqueue_scripts', 'hrs_enqueue_scripts');
  */
 function hrs_enqueue_scripts() {
 	wp_enqueue_script( 'hrs-custom', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'multi-column', get_stylesheet_directory_uri() . '/js/css3-multi-column.js', array( 'jquery' ), spine_get_script_version(), true );
 
 }
 /*
