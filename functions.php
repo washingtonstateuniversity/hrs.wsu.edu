94447dfe87302c5e7a716d89fddb0920a0878b8e
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-18 10:17:43 -0700

    Removes js load for now
    Removes js load for now

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ add_action( 'wp_enqueue_scripts', 'hrs_enqueue_scripts');
  */
 function hrs_enqueue_scripts() {
 	wp_enqueue_script( 'hrs-custom', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
-	wp_enqueue_script( 'multi-column', get_stylesheet_directory_uri() . '/js/css3-multi-column.js', array( 'jquery' ), spine_get_script_version(), true );
-
+	/*wp_enqueue_script( 'multi-column', get_stylesheet_directory_uri() . '/js/css3-multi-column.js', array( 'jquery' ), spine_get_script_version(), true );
+*/
 }
 /*
  * Add HTML5 search box
