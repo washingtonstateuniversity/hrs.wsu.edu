a2369c9c38b02bba5bce84bca3fd45a0a005b420
Adam Turner <adam.turner@wsu.edu>
2018-05-24 09:00:43 -0700

    Refactor for leaner markup

--- parts/headers.php
+++ parts/headers.php
@@ -6,39 +6,27 @@ $hrs_common_search_args = array(
 	'items_wrap'      => '<ul>%3$s</ul>',
 	'depth'           => 2,
 );
-
 ?>
-<header>
-	<div class="parent-stretch">
-		<section class="single row">
-			<div class="column one">
-				<header>
-					<div id="hr-header"><a href="http://hrs.wsu.edu">Human Resource Services</a></div>
 
-					<div class="search">
-						<button class="search-toggle" aria-expanded="false" aria-controls="search-menu">Search</button>
-						<div id="search-menu" class="expandable">
-							<form class="search-form" method="get" action="https://search.wsu.edu/Default.aspx">
-								<input name="cx" value="002970099942160159670:yqxxz06m1b0" type="hidden">
-								<input name="cof" value="FORID:11" type="hidden">
-								<input name="sa" value="Search" type="hidden">
-								<label class="screen-reader-text" for="s"><?php _e( 'Search', 'hrs-wsu-edu' ); ?></label>
-								<input class="search-input" id="s" type="search" name="q" placeholder="<?php _e( 'Search', 'hrs-wsu-edu' ); ?>" spellcheck="true" autocomplete="false" value="" />
-							</form>
-							<div class="search-links">
-								<span class="search-links-title">Common Searches</span>
-								<?php wp_nav_menu( $hrs_common_search_args ); ?>
-							</div>
-
-							<button class="screen-reader-text close-search-menu">Close menu</button>
-
-						</div>
-					</div>
-
-				</header>
+<header class="banner">
+	<section class="row single">
+		<div class="column one">
+			<a class="site-title" href="http://hrs.wsu.edu">Human Resource Services</a>
+			<button class="search-toggle" aria-expanded="false" aria-controls="search-menu">Search</button>
+		</div>
+		<div id="search-menu" class="expandable">
+			<form class="search-form" method="get" action="https://search.wsu.edu/Default.aspx">
+				<input name="cx" value="002970099942160159670:yqxxz06m1b0" type="hidden">
+				<input name="cof" value="FORID:11" type="hidden">
+				<input name="sa" value="Search" type="hidden">
+				<label class="screen-reader-text" for="s"><?php _e( 'Search', 'hrs-wsu-edu' ); ?></label>
+				<input class="search-input" id="s" type="search" name="q" placeholder="<?php _e( 'Search', 'hrs-wsu-edu' ); ?>" spellcheck="true" autocomplete="false" value="" />
+			</form>
+			<div class="search-links">
+				<p class="search-links-title">Common Searches</p>
+				<?php wp_nav_menu( $hrs_common_search_args ); ?>
+				<button class="screen-reader-text close-search-menu">Close menu</button>
 			</div>
-		</section>
-	</div>
-
-
+		</div>
+	</section>
 </header>
