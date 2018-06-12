9aea1dcc9411dd0255a5e7fb873eb9fba437386f
Adam Turner <adam.turner@wsu.edu>
2018-06-12 11:13:49 -0700

    Move feature image outside article body and remove hgroup element

--- articles/post.php
+++ articles/post.php
@@ -1,71 +1,94 @@
+<?php
+/**
+ * Article Content Template
+ *
+ * The template for displaying the contents of an individual article on either a
+ * single post page view or as part of an archive view showing a list of
+ * articles.
+ *
+ * @package WSU_Human_Resources_Services
+ * @since 0.14.0
+ */
+?>
+
 <?php $post_share_placement = spine_get_option( 'post_social_placement' ); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'article-content' ); ?>>
 
 	<header class="article-header">
-		<hgroup>
-		<?php if ( is_single() ) : ?>
-			<?php if ( true === spine_get_option( 'articletitle_show' ) ) : ?>
-				<h1 class="article-title"><?php the_title(); ?></h1>
-			<?php endif; ?>
-		<?php else : ?>
+		<?php if ( ! is_single() ) : ?>
 			<h2 class="article-title">
 				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h2>
+		<?php else : ?>
+			<?php if ( true === spine_get_option( 'articletitle_show' ) ) : ?>
+				<h1 class="article-title"><?php the_title(); ?></h1>
+			<?php endif; ?>
 		<?php endif; ?>
-		</hgroup>
-		<hgroup class="source">
-			<time class="article-date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
-			<cite class="article-author">
-				<?php
-				if ( '1' === spine_get_option( 'show_author_page' ) ) {
-					the_author_posts_link();
-				} else {
-					echo esc_html( get_the_author() );
-				}
-				?>
-			</cite>
-		</hgroup>
-
-		<?php
-		if ( is_singular() && in_array( $post_share_placement, array( 'top', 'both' ) ) ) {
-			get_template_part( 'parts/share-tools' );
-		}
-		?>
+		<time class="article-date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
 	</header>
+
+	<?php
+	if ( spine_has_thumbnail_image() ) {
+		$article_image_type = 'thumbnail-image';
+	} elseif ( spine_has_featured_image() ) {
+		$article_image_type = 'feature-image';
+		$image_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
+	}
+	?>
+
+	<?php if ( ! empty( $article_image_type ) ) : ?>
+		<figure class="article-image <?php echo esc_attr( $article_image_type ); ?>">
+			<?php
+			if ( ! is_singular() ) {
+				if ( 'thumbnail' === $article_image_type ) {
+					?><a href="<?php the_permalink(); ?>"><?php spine_the_thumbnail_image(); ?></a><?php
+				} else {
+					?><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'spine-thumbnail_size' ); ?></a><?php
+				}
+			} else {
+				?>
+				<a href="<?php esc_url( spine_get_featured_image_src() ); ?>">
+					<?php spine_the_featured_image(); ?>
+				</a>
+				<?php if ( ! empty( $image_caption ) ) { ?>
+					<figcaption class="wp-caption-text"><?php echo esc_html( $image_caption ); ?></figcaption>
+				<?php }
+			}
+			?>
+		</figure>
+	<?php endif; ?>
 
 	<?php if ( ! is_singular() ) : ?>
 		<div class="article-summary">
 			<?php
-
-			if ( spine_has_thumbnail_image() ) {
-				?><figure class="article-thumbnail"><a href="<?php the_permalink(); ?>"><?php spine_the_thumbnail_image(); ?></a></figure><?php
-			} elseif ( spine_has_featured_image() ) {
-				?><figure class="article-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'spine-thumbnail_size' ); ?></a></figure><?php
-			}
-
-			// If a manual excerpt is available, default to that. If `<!--more-->` exists in content, default
-			// to that. If an option is set specifically to display excerpts, default to that. Otherwise show
-			// full content.
+			/*
+			 * If a manual excerpt is available, default to that. If `<!--more-->` exists in content,
+			 * default to that. If an option is set specifically to display excerpts, default to that.
+			 * Otherwise show full content.
+			 */
 			if ( $post->post_excerpt ) {
-				echo get_the_excerpt() . ' <a href="' . get_permalink() . '"><span class="excerpt-more-default">&raquo; More ...</span></a>';
+				echo get_the_excerpt() . ' <a href="' . get_permalink() . '"><span class="excerpt-more-default">&rarr; More ...</span></a>';
 			} elseif ( strstr( $post->post_content, '<!--more-->' ) ) {
-				the_content( '<span class="content-more-default">&raquo; More ...</span>' );
+				the_content( '<span class="content-more-default">&rarr; More ...</span>' );
 			} elseif ( 'excerpt' === spine_get_option( 'archive_content_display' ) ) {
 				the_excerpt();
 			} else {
 				the_content();
 			}
-
 			?>
+
 		</div><!-- .article-summary -->
 	<?php else : ?>
 		<div class="article-body">
+
 			<?php the_content(); ?>
+
 			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ),
-				'after' => '</div>',
-			) );
+			// wp_link_pages( array(
+			// 	'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ),
+			// 	'after' => '</div>',
+			// ) );
 			?>
 		</div>
 	<?php endif; ?>
@@ -76,6 +99,11 @@
 			get_template_part( 'parts/share-tools' );
 		}
 		?>
+
+		<cite class="article-author">
+			<?php // The HRS Unit taxonomy will go here. ?>
+		</cite>
+
 	<?php
 	// Display site level categories attached to the post.
 	if ( has_category() ) {
