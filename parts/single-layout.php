6f9cc048182251c5e56375bce0e39c24934adf9c
Adam Turner <adam.turner@wsu.edu>
2018-06-14 09:15:25 -0700

    Fix typo

--- parts/single-layout.php
+++ parts/single-layout.php
@@ -11,7 +11,7 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<section class="row single gutter pad-ends article-containter">
+	<section class="row single gutter pad-ends article-container">
 
 		<div class="column one">
 
