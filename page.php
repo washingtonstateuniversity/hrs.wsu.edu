dff4c07c2da0ab7a4b4f649f7ada3ee9e0da8d07
Adam Turner <adam.turner@wsu.edu>
2018-08-07 11:22:05 -0700

    Adjust flow and add if posts check

--- page.php
+++ page.php
@@ -7,30 +7,31 @@
  * @package WSU_Human_Resources_Services
  * @since 0.17.0
  */
-?>
 
-<?php get_header(); ?>
+get_header();
+?>
 
 <main id="wsuwp-main" class="spine-page-default">
 
-<?php get_template_part( 'parts/headers' ); ?>
+	<?php get_template_part( 'parts/headers' ); ?>
 
-<section class="row single gutter pad-ends article-container">
+	<section class="row single gutter pad-ends article-container">
+		<div class="column one">
 
-	<div class="column one">
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					get_template_part( 'articles/article' );
+				endwhile;
+			endif;
+			?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'articles/article' ); ?>
-
-		<?php endwhile; ?>
-
-	</div><!--/column-->
-
-</section>
+		</div><!--/column-->
+	</section>
 
 	<?php get_template_part( 'parts/footers' ); ?>
 
 </main>
 
-<?php get_footer();
+<?php
+get_footer();
