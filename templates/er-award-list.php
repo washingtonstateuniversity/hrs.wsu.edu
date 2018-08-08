2ed3e4c2f49a0483859df68ca0374d57e1246e21
Adam Turner <adam.turner@wsu.edu>
2018-08-08 10:57:20 -0700

    Fix phpcs issues and match page template

--- templates/er-award-list.php
+++ templates/er-award-list.php
@@ -7,36 +7,34 @@
  * content.
  *
  * @package WSU_Human_Resources_Services
- * @since 0.11.0
+ * @since 0.20.0
  */
+
+get_header();
 ?>
 
-<?php get_header(); ?>
+<main id="wsuwp-main" class="spine-page-default">
 
-<main class="spine-page-default">
+	<?php get_template_part( 'parts/headers' ); ?>
 
-	<?php get_template_part('parts/headers'); ?>
-
-	<?php get_template_part('parts/featured-images'); ?>
-
-	<section class="row side-right gutter pad-ends">
-
+	<section class="row single gutter pad-ends article-container">
 		<div class="column one">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part('articles/article'); ?>
-
-				<?php list_erdb_awards_by_year(); ?>
-
-			<?php endwhile; ?>
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					get_template_part( 'articles/article' );
+					list_erdb_awards_by_year();
+				endwhile;
+			endif;
+			?>
 
 		</div><!--/column-->
-
 	</section>
 
 	<?php get_template_part( 'parts/footers' ); ?>
 
 </main>
 
-<?php get_footer();
+<?php
+get_footer();
