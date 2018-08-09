140ae465e99eef4445b3e8ed0cec3e62febf7ebf
Adam Turner <adam.turner@wsu.edu>
2018-08-09 09:33:09 -0700

    Switch to shortcode to display external content

---
+++ includes/shortcode-external-content.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * HRS Child Theme External Content: Shortcodes
+ *
+ * Shortcodes for fetching and displaying content from external sources.
+ *
+ * @package WSU_Human_Resources_Services
+ * @since 0.20.1
+ */
+
+namespace WSU\HRS\Shortcode_External_Content;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+add_shortcode( 'erdb_awards_list', __NAMESPACE__ . '\erdb_awards_list_shortcode' );
+
+/**
+ * Shortcode displays a list of ERDB Awards.
+ *
+ * Sample usage: [erdb_awards_list]
+ *
+ * @since 0.20.1
+ *
+ * @return string HTML content to display the latest HRS posts.
+ */
+function erdb_awards_list_shortcode() {
+	ob_start();
+
+	\WSU\HRS\Template_Tags\list_erdb_awards_by_year();
+
+	return ob_get_clean();
+}
