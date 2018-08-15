dc1369e7ac39022f9120ee4565bba4bd98af763d
Adam Turner <adam.turner@wsu.edu>
2018-08-15 11:45:31 -0700

    Add JS to filter HTML table contents based on user search input

---
+++ src/assets/js/filter.js
@@ -0,0 +1,26 @@
+'use strict';
+( function() {
+	const input = document.querySelector( 'input#search_table_input' );
+
+	function filterTable() {
+		let td, i;
+		let filter = input.value.toUpperCase();
+		let table = document.querySelector( 'table.searchable' );
+		let tr = table.getElementsByTagName( 'tr' );
+		let searchColumn = ( input.dataset.searchColumn ? input.dataset.searchColumn : 1 );
+
+		// Loop through all table rows and hide those that don't match the search.
+		for ( i = 0; i < tr.length; i++ ) {
+			td = tr[i].getElementsByTagName( 'td' )[searchColumn - 1];
+			if ( td ) {
+				if ( -1 < td.innerHTML.toUpperCase().indexOf( filter ) ) {
+					tr[i].style.display = '';
+				} else {
+					tr[i].style.display = 'none';
+				}
+			}
+		}
+	}
+
+	input.addEventListener( 'keyup', filterTable );
+}() );
