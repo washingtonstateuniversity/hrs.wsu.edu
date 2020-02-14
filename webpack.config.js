5fb231343e30dd953a3e6b58806ee09b238abbf6
Adam Turner <adam.turner@wsu.edu>
2020-02-14 11:25:29 -0800

    :boom: Revamp Webpack config to use one set of rules geared to the WP block environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,194 +1,81 @@
-const path = require( 'path' );
-
-const camelCaseDash = ( string ) => {
-	return string.replace(
-		/-([a-z])/g,
-		( match, letter ) => letter.toUpperCase()
-	);
-};
+/**
+ * External dependencies
+ */
+const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const { escapeRegExp } = require( 'lodash' );
+const { resolve, sep } = require( 'path' );
 
 /**
- * Converts @wordpress/* string request into request object.
- *
- * Note this isn't the same as camel case because of the
- * way that numbers don't trigger the capitalized next letter.
- *
- * @example
- * formatRequest( '@wordpress/api-fetch' );
- * // { this: [ 'wp', 'apiFetch' ] }
- * formatRequest( '@wordpress/i18n' );
- * // { this: [ 'wp', 'i18n' ] }
- *
- * @param {string} request Request name from import statement.
- * @return {Object} Request object formatted for further processing.
+ * WordPress dependencies
  */
-const formatRequest = ( request ) => {
-	// '@wordpress/api-fetch' -> [ '@wordpress', 'api-fetch' ]
-	const [ , name ] = request.split( '/' );
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 
-	// { this: [ 'wp', 'apiFetch' ] }
-	return {
-		this: [ 'wp', camelCaseDash( name ) ],
-	};
-};
+const isProduction = process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
 
-const wordpressExternals = ( context, request, callback ) => {
-	if ( /^@wordpress\//.test( request ) ) {
-		callback( null, formatRequest( request ), 'this' );
-	} else {
-		callback();
-	}
-};
-
-const externals = [
-	{
-		react: 'React',
-		'react-dom': 'ReactDOM',
-		moment: 'moment',
-		jquery: 'jQuery',
-		lodash: 'lodash',
-		'lodash-es': 'lodash',
-
-		// Distributed NPM packages may depend on Babel's runtime regenerator.
-		// In a WordPress context, the regenerator is assigned to the global
-		// scope via the `wp-polyfill` script. It is reassigned here as an
-		// externals to reduce the size of generated bundles.
-		//
-		// See: https://github.com/WordPress/gutenberg/issues/13890
-		'@babel/runtime/regenerator': 'regeneratorRuntime',
+const config = {
+	mode,
+	entry: {
+		index: resolve( process.cwd(), 'src/', 'index.js' ),
 	},
-	wordpressExternals,
-];
-
-module.exports = (env = {}) => {
-	return [{
-		name: 'modern',
-		mode: env.production ? 'production' : 'development',
-		devtool: env.production ? 'source-map' : 'inline-source-map',
-		entry: {
-			main: './src/js/main.js',
+	output: {
+		filename: '[name].js',
+		path: resolve( process.cwd(), 'build' ),
+	},
+	resolve: {
+		alias: {
+			'lodash-es': 'lodash',
 		},
-		output: {
-			filename: '[name].js',
-			path: path.resolve( __dirname, 'assets/js' ),
-			publicPath: '/wp-content/themes/hrs.wsu.edu/assets/js/'
-		},
-		module: {
-			rules: [
-				{
-					enforce: 'pre',
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					loader: 'eslint-loader',
-				},
-				{
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								['@babel/preset-env', {
-									corejs: 3,
-									useBuiltIns: 'entry',
-									modules: false,
-									targets: {
-										browsers: [
-											'last 2 Chrome versions', 'not Chrome < 60',
-											'last 2 Safari versions', 'not Safari < 10.1',
-											'last 2 iOS versions', 'not iOS < 10.3',
-											'last 2 Firefox versions', 'not Firefox < 54',
-											'last 2 Edge versions', 'not Edge < 15',
-										]
-									}
-								}]
-							],
-							plugins: ['@babel/syntax-dynamic-import']
-						}
-					}
-				}
-			]
-		}
-	}, {
-		name: 'wpblocks',
-		mode: env.production ? 'production' : 'development',
-		devtool: env.production ? 'source-map' : 'inline-source-map',
-		entry: {
-			blocks: './src/js/blocks.js'
-		},
-		output: {
-			filename: '[name].js',
-			path: path.resolve( __dirname, 'assets/js' ),
-			publicPath: '/wp-content/themes/hrs.wsu.edu/assets/js/'
-		},
-		externals,
-		module: {
-			rules: [
-				{
-					enforce: 'pre',
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					loader: 'eslint-loader',
-				},
-				{
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								['@wordpress/babel-preset-default', {
-									corejs: 3,
-									useBuiltIns: 'entry',
-									modules: false,
-									targets: {
-										browsers: [
-											'last 2 Chrome versions', 'not Chrome < 60',
-											'last 2 Safari versions', 'not Safari < 10.1',
-											'last 2 iOS versions', 'not iOS < 10.3',
-											'last 2 Firefox versions', 'not Firefox < 54',
-											'last 2 Edge versions', 'not Edge < 15',
-										]
-									}
-								}]
-							],
-							plugins: ['@babel/syntax-dynamic-import']
-						}
-					}
-				}
-			]
-		}
-	}, {
-		name: 'legacy',
-		mode: env.production ? 'production' : 'development',
-		devtool: env.production ? 'source-map' : 'inline-source-map',
-		entry: {
-			main: './src/js/main-legacy.js'
-		},
-		output: {
-			filename: '[name].es5.js',
-			path: path.resolve( __dirname, 'assets/js' ),
-			publicPath: '/wp-content/themes/hrs.wsu.edu/assets/js/'
-		},
-		module: {
-			rules: [{
-				test: /\.m?js$/,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							['@babel/preset-env', {
-								corejs: 3,
-								useBuiltIns: 'entry',
-								modules: false
-							}]
-
-						],
-						plugins: ['@babel/syntax-dynamic-import']
-					}
-				}
-			}]
-		}
-	}];
+				use: [
+					require.resolve( 'thread-loader' ),
+					{
+						loader: require.resolve( 'babel-loader' ),
+						options: {
+							// Babel uses a directory within local node_modules
+							// by default. Use the environment variable option
+							// to enable more persistent caching.
+							cacheDirectory: process.env.BABEL_CACHE_DIRECTORY || true,
+							presets: [ require.resolve( '@wordpress/babel-preset-default' ) ],
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		// WP_BUNDLE_ANALYZER global variable enables utility that represents
+		// bundle content as a convenient interactive zoomable treemap.
+		process.env.WP_BUNDLE_ANALYZER && new BundleAnalyzerPlugin(),
+		new CopyWebpackPlugin( [
+			{
+				from: './src/templates/**/index.php',
+				test: new RegExp( `([\\w-]+)${ escapeRegExp( sep ) }index\\.php$` ),
+				to: 'templates/[1].php',
+			},
+		] ),
+		new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
+	].filter( Boolean ),
+	stats: {
+		children: false,
+	},
 };
+
+if ( ! isProduction ) {
+	// WP_DEVTOOL global variable controls how source maps are generated.
+	// See: https://webpack.js.org/configuration/devtool/#devtool.
+	config.devtool = process.env.WP_DEVTOOL || 'source-map';
+	config.module.rules.unshift( {
+		test: /\.js$/,
+		use: require.resolve( 'source-map-loader' ),
+		enforce: 'pre',
+	} );
+}
+
+module.exports = config;
