4eb46b0856b9cbd67e895a2545e6ae2cc774ea51
Adam Turner <adam.turner@wsu.edu>
2020-03-04 10:07:06 -0800

    Add `src/editor.js` to handle all backend imports

---
+++ src/editor.js
@@ -0,0 +1,15 @@
+/**
+ * Internal dependencies
+ */
+import initEditorComponents from './components/editor';
+
+/**
+ * Initializes all of the site submodules.
+ *
+ * @since 2.0.0
+ */
+function editor() {
+	initEditorComponents();
+}
+
+editor();
