b513b52d22143d76188e1945e772671d9f897f4a
Adam Turner <adam.turner@wsu.edu>
2020-03-04 10:48:01 -0800

    :alembic: Move search menu PHP into component directory and include in header

--- parts/before-main.php
+++ parts/before-main.php
@@ -11,29 +11,13 @@
  * @since 0.14.0
  */
 
-// Arguments for the WP nav menu in the header search menu.
-$hrs_common_search_args = array(
-	'theme_location' => 'hrs-search-menu',
-	'menu'           => 'hrs-search-menu',
-	'container'      => 'nav',
-	'items_wrap'     => '<ul>%3$s</ul>',
-	'depth'          => 2,
-);
 ?>
-
 <header class="site-header" aria-label="hrs-website">
 	<section class="row single">
 		<div class="site-banner column one">
 			<a class="site-title" href="<?php echo esc_url( home_url() ); ?>">Human Resource Services</a>
 			<button class="search-toggle" aria-expanded="false" aria-controls="search-menu">Search</button>
 		</div>
-		<div id="search-menu" class="expandable">
-			<?php get_search_form(); ?>
-			<div class="search-links">
-				<p class="search-links-title">Common Searches</p>
-				<?php wp_nav_menu( $hrs_common_search_args ); ?>
-				<button class="screen-reader-text close-search-menu">Close menu</button>
-			</div>
-		</div>
+		<?php get_template_part( '/build/components/search-menu' ); ?>
 	</section>
 </header>
