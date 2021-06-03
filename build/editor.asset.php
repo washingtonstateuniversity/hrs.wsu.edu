de56d742457cf263ba9091f208c46a99fa362558
Adam Turner <act@adamturner.dev>
2021-06-03 10:22:14 -0700

    :package: Update built files

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5723cd06039c2604c16706909d8d4323');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c502de5eeacf4bc8977976f6d9b7ac74');
