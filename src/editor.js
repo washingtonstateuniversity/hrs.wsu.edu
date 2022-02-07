37963728f9e90859900621c9db8a234294b8283d
Adam Turner <act@adamturner.dev>
2022-02-07 10:04:43 -0800

    Removes some unused block editor panels

--- src/editor.js
+++ src/editor.js
@@ -1,8 +1,20 @@
 /**
+ * WordPress dependencies
+ */
+const { dispatch } = wp.data;
+
+/**
  * Internal dependencies
  */
 import { modifyBlockStyles, unregisterBlocks } from './components/editor';
 import registerDisplayOptions from './lib/block-filters';
+
+function removeEditorPanels() {
+	const { removeEditorPanel } = dispatch( 'core/edit-post' );
+
+	removeEditorPanel( 'discussion-panel' );
+	removeEditorPanel( 'template' );
+}
 
 /**
  * Initializes all of the site submodules.
@@ -10,6 +22,7 @@ import registerDisplayOptions from './lib/block-filters';
  * @since 2.0.0
  */
 registerDisplayOptions();
+removeEditorPanels();
 wp.domReady( () => {
 	modifyBlockStyles();
 	unregisterBlocks();
